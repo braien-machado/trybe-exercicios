a21886e2bdca93cacb3c0db2ebdabe9cedaebf95
Braien <braienmp@outlook.com>
2021-07-28 19:31:57 -0300

    resolve exercicio 9

---
+++ trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-1-javascript-primeiros-passos/exercicio9.js
@@ -0,0 +1,10 @@
+let numero1 = 1;
+let numero2 = 3;
+let numero3 = 2;
+
+if (numero1 % 2 != 0 || numero2 % 2 != 0 || numero3 % 2 != 0) {
+  console.log('true');
+}
+else {
+  console.log('false');
+}
