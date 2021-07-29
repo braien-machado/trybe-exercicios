2f84d3df50a241eabaadec1f31351c26f7bedaa7
Braien <braienmp@outlook.com>
2021-07-29 19:30:34 -0300

    resolve exercicio 7

--- trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-2-javascript-array-e-loop-for/exercicio7.js
+++ trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-2-javascript-array-e-loop-for/exercicio7.js
@@ -1,16 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let numberOfOdds = 0;
+let smallestNumber = numbers[0];
 
-for(let i = 0; i < numbers.length; i += 1) {
-  if(numbers[i] % 2 != 0){
-    numberOfOdds += 1;
+for(let i = 1; i < numbers.length; i += 1) {
+  if(numbers[i] < smallestNumber){
+    smallestNumber = numbers[i];
   }
   else{}
 }
 
-if(numberOfOdds > 0) {
-  console.log(numberOfOdds);
-}
-else {
-  console.log('nenhum valor ímpar encontrado')
-}
+console.log(smallestNumber);
