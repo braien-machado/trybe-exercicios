87ca28a98682764e0534e04dd9df1b79ccb8d243
Braien <braienmp@outlook.com>
2021-07-29 19:27:06 -0300

    resolve exercicios 5-7 e altera nome de variaveis nos exercicios 2-4

--- trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-2-javascript-array-e-loop-for/exercicio3.js
+++ trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-2-javascript-array-e-loop-for/exercicio3.js
@@ -1,10 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let sum = 0;
 let divisor = numbers.length;
 
 for(let i = 0; i < numbers.length; i += 1) {
-  soma += numbers[i];
+  sum += numbers[i];
 }
 
-let mediaAritmetica = soma / divisor;
-console.log(mediaAritmetica);
+let average = sum / divisor;
+console.log(average);
--- trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-2-javascript-array-e-loop-for/exercicio4.js
+++ trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-2-javascript-array-e-loop-for/exercicio4.js
@@ -1,10 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let sum = 0;
 let divisor = numbers.length;
 
 for(let i = 0; i < numbers.length; i += 1) {
-  soma += numbers[i];
+  sum += numbers[i];
 }
 
-let mediaAritmetica = soma / divisor;
-console.log(mediaAritmetica);
+let average = sum / divisor;
+if(average > 20) {
+  console.log('valor maior que 20');
+}
+else {
+  console.log('valor menor que 20');
+}
