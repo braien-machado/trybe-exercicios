3c7ea812510edaaeb8f1dc96ec678f620e2954f2
Braien <braienmp@outlook.com>
2021-07-31 10:31:35 -0300

    altera exercicio3.js

--- trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio3.js
+++ trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio3.js
@@ -1,15 +1,11 @@
 let n = 5;
 let rightTriangleOfAsteriskRight = '';
-if (n <=1){
-  console.log("Error. 'n' must be greater than 1");
-} else {
-  for (let index = 1; index <= n; index += 1) {
+for (let index = 1; index <= n; index += 1) {
     rightTriangleOfAsteriskRight += '*';
     let spaces = ''
-    for (let indexSpaces = n-index; indexSpaces >0; indexSpaces -= 1) {
+    for (let indexSpaces = n-index; indexSpaces > 0; indexSpaces -= 1) {
       spaces += ' ';
     }
     let rightTriangleSpaces = spaces + rightTriangleOfAsteriskRight;
     console.log(rightTriangleSpaces);
   }
-}
