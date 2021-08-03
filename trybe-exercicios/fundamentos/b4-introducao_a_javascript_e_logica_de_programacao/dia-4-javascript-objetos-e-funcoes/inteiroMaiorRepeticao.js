2dbfa80ccb1ba16ba3f9206f14a380c2aa4dab26
Braien <braienmp@outlook.com>
2021-08-02 23:28:17 -0300

    resolve exercicio 6 da parte 2

--- trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-4-javascript-objetos-e-funcoes/inteiroMaiorRepeticao.js
+++ trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-4-javascript-objetos-e-funcoes/inteiroMaiorRepeticao.js
@@ -1,6 +1,5 @@
 function maisRepetido(numeros) {
   let num = {};
-
   for (let index = 0; index < numeros.length; index += 1) {
     let valor = numeros[index];
     if (num[valor] === undefined) {
@@ -9,18 +8,15 @@ function maisRepetido(numeros) {
       num[valor] = num[valor] + 1;
     }
   }
-
-  let contRepetido = 0;
-  let contNumero = 0;
-
-  for (let prop in num) {
-    if (contRepetido < num[prop]) {
-      contRepetido = num[prop];
-      contNumero = prop;
+  let numeroMaisRepetido = 0;
+  let numeroRepeticao = 0;
+  for (let key in num) {
+    if (numeroRepeticao < num[key]) {
+      numeroMaisRepetido = key;
+      numeroRepeticao = num[key];
     }
   }
-
-  return contNumero;
+  return numeroMaisRepetido;
 }
 
-console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3])); //2
+console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3]));
