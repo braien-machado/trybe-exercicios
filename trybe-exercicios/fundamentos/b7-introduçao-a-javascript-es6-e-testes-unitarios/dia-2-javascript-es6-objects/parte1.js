5fdf26520c1503ebbbdf311a58c9212eeb3f4691
Braien <braienmp@outlook.com>
2021-08-30 19:14:04 -0300

    finaliza exercicio 2 da parte 1

--- trybe-exercicios/fundamentos/b7-introduçao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte1.js
+++ trybe-exercicios/fundamentos/b7-introduçao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte1.js
@@ -34,16 +34,25 @@ const order = {
   },
 };
 
-const customerInfo = (order) => {
-  // Adicione abaixo as informações necessárias.
+/** Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701". */
 
+const customerInfo = (order) => {
+  const str  = `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone:${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP:${order.address.apartment}`
+
+  console.log(str);
 }
 
 customerInfo(order);
 
-const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
+/** Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00." */
 
+const orderModifier = (order) => {
+  const newBuyer = order.name = 'Luiz Silva';
+  const total = order.payment.total = 50;
+  const pizzas = Object.keys(order['order'].pizza);
+  const drinks = order['order'].drinks.coke.type;
+
+  console.log(`Olá ${newBuyer}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drinks} é R$ ${total},00.`)
 }
 
 orderModifier(order);
