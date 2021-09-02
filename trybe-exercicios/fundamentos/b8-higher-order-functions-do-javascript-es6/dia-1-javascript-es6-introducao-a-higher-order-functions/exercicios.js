1a61eff497dacdbb748337fa1f001ecf421e6789
Braien <braienmp@outlook.com>
2021-09-02 16:11:06 -0300

    finaliza exercicio 2 do dia 8.1

--- trybe-exercicios/fundamentos/b8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
+++ trybe-exercicios/fundamentos/b8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
@@ -22,4 +22,12 @@ console.log(newEmployees(personalInfo));
 
 // 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
+const compareNumbers = (num1, num2) => (num1 === num2)? 'Parabéns você ganhou' : 'Tente novamente';
 
+const prizeDraw = (number, callback) => {
+  const randomNumber = Math.ceil(Math.random() * 5)
+  const result = callback(number, randomNumber);
+  return result;
+}
+
+console.log(prizeDraw(2, compareNumbers))
