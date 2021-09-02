83b01b8ae3f06b2f92df34c2085f52d31c1e4a4e
Braien <braienmp@outlook.com>
2021-09-02 19:06:22 -0300

    implementa dano dragao

--- trybe-exercicios/fundamentos/b8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
+++ trybe-exercicios/fundamentos/b8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/bonus.js
@@ -1,6 +1,6 @@
 /* Parte I - Game Actions Simulator
 Nestes exercícios você irá implementar HOFs que simulam um turno de batalha em um jogo. Você irá criar funções que calculam dano, atualizam status, e ao final, retornam os resultados da rodada.
-Para os próximos exercícios copie o código abaixo.
+Para os próximos exercícios copie o código abaixo. */
 
 const mage = {
   healthPoints: 130,
@@ -22,11 +22,17 @@ const dragon = {
   damage: undefined,
 };
 
-const battleMembers = { mage, warrior, dragon }; */
+const battleMembers = { mage, warrior, dragon };
+
+function randomDamage(min, max) {
+  const damage = Math.round(Math.random() * (max-min) + min);
+  console.log(damage);
+  return damage;
+}
 
 // 1 - Crie uma função que retorna o dano do dragão.
 // O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
-
+const damageDragon = randomDamage(15, dragon.strength);
 // 2 - Crie uma função que retorna o dano causado pelo warrior .
 // O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
 
