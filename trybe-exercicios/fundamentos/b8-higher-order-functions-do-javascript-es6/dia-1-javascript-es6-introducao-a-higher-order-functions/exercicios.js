bb9b2822a7b8cfef0f752e82344ccbd1ac3e35da
Braien <braienmp@outlook.com>
2021-09-02 16:00:22 -0300

    inicia exercicios do dia 8.1 e finaliza exercicio 1

---
+++ trybe-exercicios/fundamentos/b8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios.js
@@ -0,0 +1,25 @@
+// 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'),
+    id2: callback('Luiza Drumond'),
+    id3: callback('Carla Paiva'),
+  }
+  return employees;
+};
+
+const getEmail = (string) => {
+  const strChanged = string.toLowerCase().replace(/\s/g, '_');
+  const email = `${strChanged}@trybe.com`;
+  return email;
+}
+
+const personalInfo = (nomeCompleto) => ({
+  nome: nomeCompleto,
+  email: getEmail(nomeCompleto),
+});
+console.log(newEmployees(personalInfo));
+
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+
