bee50cc442a83b98189b546a6ec8437db89cf3d1
Braien <braienmp@outlook.com>
2021-09-09 20:36:34 -0300

    altera exercicio 6 de 8.4

--- trybe-exercicios/fundamentos/b8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise6.js
+++ trybe-exercicios/fundamentos/b8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise6.js
@@ -7,12 +7,10 @@ const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
   // escreva seu código aqui
-  return students.map((student, index) => {
-    return {
+  return students.map((student, index) => ({
       name: student,
       average: grades[index].reduce((average, grade) => average + grade) / grades[index].length
-  };
-  })
+  }))
 }
 const expected = [
   { name: 'Pedro Henrique', average: 7.8 },
