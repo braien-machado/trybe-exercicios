969c1cc90d94755ece21bff5ab9aa6e9f44c3cb6
Braien <braienmp@outlook.com>
2021-10-05 22:05:12 -0300

    renomeia diretorios exercicios dia 1 bloco 10

--- trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-1-javascript-primeiros-passos/exercicio01.js
+++
@@ -1,8 +0,0 @@
-let numero1 = 5;
-let numero2 = 2;
-
-console.log( 'A soma de ' + numero1 + ' e ' + numero2 + ' é: ' + (numero1 + numero2) );
-console.log( 'A subtração de ' + numero1 + ' por ' + numero2 + ' é: ' + (numero1 - numero2) );
-console.log( 'A multiplicação de ' + numero1 + ' por ' + numero2 + ' é: ' + (numero1 * numero2) );
-console.log( 'A divisão de ' + numero1 + ' por ' + numero2 + ' é: ' + (numero1 / numero2) );
-console.log( 'O resto da divisão de ' + numero1 + ' por ' + numero2 + ' é: ' + (numero1 % numero2) );
--- trybe-exercicios/fundamentos/b4-introducao_a_javascript_e_logica_de_programacao/dia-2-javascript-array-e-loop-for/exercicio8.js
+++
@@ -1,7 +0,0 @@
-let arrayNumbers = [];
-
-for(let i = 1; i <= 25; i += 1) {
-  arrayNumbers.push(i);
-}
-
-console.log(arrayNumbers);
