cabd0d76830464608acc767cd11e687a0c45536d
Braien <braienmp@outlook.com>
2021-10-11 12:43:54 -0300

    realiza exercicio 11.2

---
+++ trybe-exercicios/02-front-end/b11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+import './App.css';
+import Form from './Form.js'
+import FormError from './FormError.js'
+import FormDataDisplay from './FormDataDisplay.js'
+
+const INITIAL_STATE = {
+  name: '',
+  email: '',
+  cpf:'',
+  address:'',
+  city:'',
+  countryState: '',
+  addressType: '',
+  resume:'',
+  role:'',
+  roleDescription: '',
+  formError: {},
+  submitted: false,
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = INITIAL_STATE;
+  }
+
+  changeHandler = event => {
+    let {name, value} = event.target;
+
+    if (name === 'name') value=value.toUpperCase();
+    if(name==='address') value=this.validateAddress(value);
+
+    this.updateState(name,value);
+  }
+
+  onBlurHandler = event => {
+    let {name, value} = event.target;
+
+    if(name==='city') value=value.match(/^\d/) ? '' : value;
+
+    this.updateState(name,value);
+  }
+
+  updateState = (name,value) => {
+    this.setState((state) => ({
+      [name]:value,
+      formError: {
+        ...state.formError,
+        [name]: this.validateField(name, value)
+      }
+    }))
+  }
+
+  validateAddress = address => address.replace(/[^\w\s]/gi, '')
+
+  handleSubmit = event => event.preventDefault();
+
+  validateField(fieldName, value) {
+    switch (fieldName) {
+      case 'email':
+        const isValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{3})$/i);
+        return isValid? '' : ' is invalid';
+      default:
+        break;
+    }
+    return '';
+  }
+
+  resetForm = () => this.setState(INITIAL_STATE);
+
+  sendForm = () => {this.setState({submitted: true})};
+
+  render(){
+    const {submitted} = this.state;
+
+    return (
+      <main>
+        <Form
+          sendForm={this.sendForm}
+          resetForm={this.resetForm}
+          changeHandler={this.changeHandler}
+          currentState={this.state}
+          onBlurHandler={this.onBlurHandler}
+        />
+        <div className='container'>
+          <FormError formError={this.state.formError} />
+        </div>
+        { submitted && <FormDataDisplay currentState={this.state} />}
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ trybe-exercicios/02-front-end/b11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/Form.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PersonalForm from './PersonalForm'
+import ProfessionalForm from './ProfessionalForm'
+
+class Form extends Component {
+  render() {
+    const {sendForm, resetForm, changeHandler, currentState, onBlurHandler} = this.props;
+    return (
+      <form>
+        <PersonalForm
+          changeHandler={changeHandler}
+          onBlurHandler={onBlurHandler}
+          currentState={currentState}
+        />
+        <ProfessionalForm changeHandler={changeHandler} />
+        <input
+          type='button'
+          onClick={sendForm}
+          value='Enviar'
+        />
+        <input
+          type='button'
+          onClick={resetForm}
+          value='Limpar'
+        />
+      </form>
+    )
+  }
+}
+
+export default Form;
