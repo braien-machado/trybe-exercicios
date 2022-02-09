62c7a4973555a5634609c8daf40c33b781affb8b
Braien Machado de Paula <braienmp@outlook.com>
2022-02-09 20:42:50 -0300

    adapta exercicio1.js para resolver exercicio 2

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios/exercicio1.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicios/exercicio1.js
@@ -1,3 +1,9 @@
+// 1. Crie uma função que recebe três parâmetros retorna uma Promise .
+// Caso algum dos parâmetros recebidos não seja um número, rejeite a Promise com o motivo "Informe apenas números" .
+// Caso todos os parâmetros sejam numéricos, some os dois primeiros e multiplique o resultado pelo terceiro ( (a + b) * c ).
+// Caso o resultado seja menor que 50, rejeite a Promise com o motivo "Valor muito baixo"
+// Caso o resultado seja maior que 50, resolva a Promise com o valor obtido.
+
 const doMath = (a, b, c) => {
   const promise = new Promise((resolve, reject) => {
     if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
@@ -12,20 +18,22 @@ const doMath = (a, b, c) => {
   return promise;
 };
 
-doMath(2, 2, 15)
-  .then(result => console.log(result))
-  .catch(err => console.error(err.message));
+// doMath(2, 2, 15)
+//   .then(result => console.log(result))
+//   .catch(err => console.error(err.message));
 
 // Resultado: 60
 
-doMath(1, 2, 15)
-  .then(result => console.log(result))
-  .catch(err => console.error(err.message));
+// doMath(1, 2, 15)
+//   .then(result => console.log(result))
+//   .catch(err => console.error(err.message));
 
 // Resultado: 'Valor muito baixo'
 
-doMath('teste', 2, 15)
-  .then(result => console.log(result))
-  .catch(err => console.error(err.message));
+// doMath('teste', 2, 15)
+//   .then(result => console.log(result))
+//   .catch(err => console.error(err.message));
 
 // Resultado: 'Informe apenas números'
+
+module.exports = doMath;
