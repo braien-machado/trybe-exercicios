a2f6b57dd45364501885e581296fc3b5dfa2cf3f
Braien Machado de Paula <braienmp@outlook.com>
2022-02-10 19:14:31 -0300

    corrige retorno para o caso de parametro nao numerico

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-backend-com-nodejs/exercicios/exercicio1/checkNumber.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-backend-com-nodejs/exercicios/exercicio1/checkNumber.js
@@ -1,5 +1,5 @@
 function checkNumber(number) {
-  if (typeof number !== 'number') throw new Error('The parameter must be a number!');
+  if (typeof number !== 'number') return 'o valor deve ser um número';
 
   if (number > 0) return 'positivo';
   if (number < 0) return 'negativo';
