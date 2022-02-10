3fd13d76f6cbed37714a5ad996be1123af9e3f08
Braien Machado de Paula <braienmp@outlook.com>
2022-02-10 19:33:39 -0300

    escreve testes iniciais de exercicio 2

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-backend-com-nodejs/exercicios/exercicio2/tests/writeFile.test.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-backend-com-nodejs/exercicios/exercicio2/tests/writeFile.test.js
@@ -5,4 +5,20 @@ describe('test', () => {
   it('if the import is a function', () => {
     expect(writeFile).to.be.a('function');
   });
+
+  describe('if the file does not exist', () => {
+    it('throws an error', () => {
+      expect(() => writeFile('no-file', '')).to.throw('O arquivo não existe');
+    });
+  });
+
+  describe('if the file exists', () => {
+    it('returns a string', () => {
+      expect(writeFile('./exercise2.txt', 'it works!')).to.be.a('string');
+    })
+    
+    it('returns \'ok\'', () => {
+      expect(writeFile('./exercise2.txt', 'it works!')).to.be.equal('ok');
+    })
+  })
 });
