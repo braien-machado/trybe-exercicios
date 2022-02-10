9472ce09d02682214aab51611a35c5e62d23275b
Braien Machado de Paula <braienmp@outlook.com>
2022-02-10 20:39:03 -0300

    isola testes com sinon

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-backend-com-nodejs/exercicios/exercicio2/tests/writeContentInFile.test.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-backend-com-nodejs/exercicios/exercicio2/tests/writeContentInFile.test.js
@@ -9,29 +9,34 @@
 // Adapte os testes adicionando stub ao módulo utilizado do fs , isolando assim o teste.
 // Garanta que todos os testes escritos no exercício 4 irão passar com sucesso.
 
+const fs = require('fs');
+const sinon = require('sinon');
 const { expect } = require('chai');
 const writeContentInFile = require('../writeContentInFile');
 
 describe('test', () => {
+  before(() => {
+    sinon.stub(fs, 'writeFileSync');
+  })
+
+  after(() => {
+    fs.writeFileSync.restore();
+  })
+
   it('if the import is a function', () => {
     expect(writeContentInFile).to.be.a('function');
   });
 
-  // describe('if an error occurs', () => {
-  //   it('throws an error', () => {
-  //     expect(() => writeContentInFile('', '')).to.throw(`Erro ao escrever o arquivo: err.message`);
-  //   });
-  // });
-
-  describe('if the file exists', () => {
-    const result = writeContentInFile('../exercise2.txt', 'it works!');
+  describe('if the function', () => {
+    const result = writeContentInFile('./exercise2.txt', 'it works!');
 
     it('returns a string', () => {
       expect(result).to.be.a('string');
     })
     
     it('returns \'ok\'', () => {
-      expect(result).to.be.equal('ok');
+      const resultExpected = 'ok'
+      expect(result).to.be.equal(resultExpected);
     })
   })
 });
