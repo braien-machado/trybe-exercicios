0f466905fe11179eb366803bbd34f02f02021116
Braien Machado de Paula <braienmp@outlook.com>
2022-02-10 18:58:28 -0300

    testa se a funcao retorna uma string ao usar parametro numerico

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-backend-com-nodejs/exercicios/exercicio1/checkNumber.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-backend-com-nodejs/exercicios/exercicio1/checkNumber.js
@@ -1,7 +1,7 @@
 function checkNumber(number) {
   if (typeof number !== 'number') throw new Error('The parameter must be a number!');
+
+  return '';
 }
-
-
 
 module.exports = checkNumber;
--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-backend-com-nodejs/exercicios/exercicio1/tests/checkNumber.test.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-backend-com-nodejs/exercicios/exercicio1/tests/checkNumber.test.js
@@ -4,10 +4,19 @@ const checkNumber = require('../checkNumber');
 describe('test', () => {
   it('if the import is a function', () => {
     expect(checkNumber).to.be.a('function');
+  });
+
+  describe('if the parameter is not a number,', () => {
+    it('throw error', () => {
+      const err = 'The parameter must be a number!';
+
+      expect(() => checkNumber('2')).to.throw(err);
+    });
   })
 
-  it('if the parameter is not a number, throw error', () => {
-    const err = 'The parameter must be a number!'
-    expect(() => checkNumber('')).to.throw(err);
-  })
+  describe('if the parameter is a number,', () => {
+    it('it returns a string', () => {
+      expect(checkNumber(2)).to.be.a('string')
+    });
+  });
 });
