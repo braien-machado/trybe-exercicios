655f3722446056fbf84ab9120c4240fb4aa50f5e
Braien Machado de Paula <braienmp@outlook.com>
2022-02-11 20:05:23 -0300

    adiciona requisicao com metodo PUT para atualizar receitas

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/examples/lista-de-receitas/index.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/examples/lista-de-receitas/index.js
@@ -75,6 +75,18 @@ app.get('/recipes/:id', function (req, res) {
   res.status(200).json(recipe);
 });
 
+app.put('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+})
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
