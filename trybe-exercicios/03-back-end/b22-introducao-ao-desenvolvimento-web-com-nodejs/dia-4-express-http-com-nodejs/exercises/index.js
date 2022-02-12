62aea2e0c063e2dc75b046b37476a7d2fb975b04
Braien Machado de Paula <braienmp@outlook.com>
2022-02-12 10:25:47 -0300

    remove tratamento de de requisiçao sem 'name' no exercicio 2

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
@@ -19,10 +19,13 @@ app.get('/ping', function (_req, res) {
 app.post('/hello', function (req,res) {
   const { name } = req.body;
 
-  if (!name) return res.status(400).json({ message: `'name' não foi definido!` });
-
   res.status(200).json({ message: `Hello, ${name}!` });
 });
+
+// Crie uma rota POST /greetings
+// Sua rota deve receber o seguinte JSON: { "name": "<nome do usuário>", "age": <idade do usuário> } .
+// Caso a pessoa usuária tenha idade superior a 17 anos, devolva o JSON { "message": "Hello, <nome do usuário>!" } com o status code 200 - OK .
+// Caso a pessoa usuária tenha 17 anos ou menos, devolva o JSON { "message": "Unauthorized" } com o status code 401 - Unauthorized .
 
 app.listen(3002, () => {
   console.log('Aplicativo ouvindo na porta 3002');
