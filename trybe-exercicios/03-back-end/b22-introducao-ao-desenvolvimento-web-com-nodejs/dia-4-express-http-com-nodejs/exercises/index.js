c51ca594fe890f1b97cfc7602defb3b0b3b8254d
Braien Machado de Paula <braienmp@outlook.com>
2022-02-12 10:49:55 -0300

    resolve exercicio 4

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
@@ -22,7 +22,7 @@ app.post('/hello', function (req,res) {
   res.status(200).json({ message: `Hello, ${name}!` });
 });
 
-// Crie uma rota POST /greetings
+// 3. Crie uma rota POST /greetings
 // Sua rota deve receber o seguinte JSON: { "name": "<nome do usuário>", "age": <idade do usuário> } .
 // Caso a pessoa usuária tenha idade superior a 17 anos, devolva o JSON { "message": "Hello, <nome do usuário>!" } com o status code 200 - OK .
 // Caso a pessoa usuária tenha 17 anos ou menos, devolva o JSON { "message": "Unauthorized" } com o status code 401 - Unauthorized .
@@ -33,6 +33,15 @@ app.post('/greetings', function (req, res) {
   if (age <= 17) return res.status(401).json({ message: 'Unauthorized' });
 
   res.status(200).json({ message: `Hello, ${name}!` });
+});
+
+// 4. Crie uma rota PUT /users/:name/:age .
+// Sua rota deve retornar o seguinte JSON: { "message": "Seu nome é <name> e você tem <age> anos de idade" } .
+
+app.put('/users/:name/:age', function (req, res) {
+  const { name, age } = req.params;
+
+  res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
 });
 
 app.listen(3002, () => {
