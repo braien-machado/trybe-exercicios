54f872423709abaf501cc272c5dde61b9e0926bd
Braien Machado de Paula <braienmp@outlook.com>
2022-02-12 10:24:43 -0300

    utiliza body-parser e realiza exercicio 2

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercises/index.js
@@ -3,11 +3,25 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 // 1. Crie uma rota GET /ping
 // Sua rota deve retornar o seguinte JSON: { message: 'pong' }
 
-app.get('/ping', function (req, res) {
+app.get('/ping', function (_req, res) {
   res.status(200).json({ message: 'pong' });
+});
+
+// 2. Crie uma rota POST /hello
+// Sua rota deve receber, no body da requisição, o seguinte JSON: { "name": "<nome do usuário>" }
+// Sua rota deve retornar o seguinte JSON: { "message": "Hello, <nome do usuário>!" } .
+
+app.post('/hello', function (req,res) {
+  const { name } = req.body;
+
+  if (!name) return res.status(400).json({ message: `'name' não foi definido!` });
+
+  res.status(200).json({ message: `Hello, ${name}!` });
 });
 
 app.listen(3002, () => {
