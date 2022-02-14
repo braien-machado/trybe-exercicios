d19536806c26efcddc6bac6d3a5811863f39de34
Braien Machado de Paula <braienmp@outlook.com>
2022-02-14 12:15:13 -0300

    adiciona enunciado de Bonus 1 ao arquivo authMiddleware.js

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpsons-api/authMiddleware.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpsons-api/authMiddleware.js
@@ -1,3 +1,8 @@
+// Bonus 1. Adicione autenticação a todos os endpoints.
+// O token deve ser enviado através do header Authorization .
+// O token deve ter exatamente 16 caracteres.
+// Caso o token seja inválido ou inexistente, a resposta deve possuir o status 401 - Unauthorized e o JSON { message: 'Token inválido!' } .
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
