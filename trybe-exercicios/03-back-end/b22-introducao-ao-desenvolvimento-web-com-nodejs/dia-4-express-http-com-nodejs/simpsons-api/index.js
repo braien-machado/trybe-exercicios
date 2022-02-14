6bdb0345d1ebd11483b7d923293bab0cbb812f39
Braien Machado de Paula <braienmp@outlook.com>
2022-02-14 10:51:59 -0300

    cria metodo get para path simpsons/:id

--- trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpsons-api/index.js
+++ trybe-exercicios/03-back-end/b22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpsons-api/index.js
@@ -1,14 +1,82 @@
+// 5. Crie uma API de dados das personagens de Simpsons
+// Crie um arquivo chamado simpsons.json e popule com os seguintes dados:
+
+// [
+//   {
+//     "id": "1",
+//     "name": "Homer Simpson"
+//   },
+//   {
+//     "id": "2",
+//     "name": "Marge Simpson"
+//   },
+//   {
+//     "id": "3",
+//     "name": "Bart Simpson"
+//   },
+//   {
+//     "id": "4",
+//     "name": "Lisa Simpson"
+//   },
+//   {
+//     "id": "5",
+//     "name": "Maggie Simpson"
+//   },
+//   {
+//     "id": "6",
+//     "name": "Ned Flanders"
+//   },
+//   {
+//     "id": "7",
+//     "name": "Montgomery Burns"
+//   },
+//   {
+//     "id": "8",
+//     "name": "Nelson Muntz"
+//   },
+//   {
+//     "id": "9",
+//     "name": "Krusty"
+//   },
+//   {
+//     "id": "10",
+//     "name": "Milhouse Van Houten"
+//   }
+// ]
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 
 const app = express();
 
-app.get('/simpsons', async function (req, res) {
+// 6. Crie um endpoint GET /simpsons
+// O endpoint deve retornar um array com todos os simpsons.
+
+app.get('/simpsons', async function (_req, res) {
   fs.readFile('./simpsons.json', 'utf8')
     .then((result) => {
       const array = JSON.parse(result);
       res.status(200).json(array);
+    })
+    .catch((err) => res.status(500).json({message: err.message}));
+});
+
+// 7. Crie um endpoint GET /simpsons/:id
+// O endpoint deve retornar o personagem com o id informado na URL da requisição.
+// Caso não exista nenhum personagem com o id especificado, retorne o JSON { message: 'simpson not found' } com o status 404 - Not Found .
+
+app.get('/simpsons/:id', function (req, res) {
+  const { id } = req.params;
+  fs.readFile('./simpsons.json', 'utf8')
+    .then((result) => {
+      const array = JSON.parse(result);
+
+      const character = array.find((char) => char.id === id);
+
+      if (!character) return res.status(404).json({message: 'simpson not found'});
+
+      res.status(200).json(character);
     })
     .catch((err) => res.status(500).json({message: err.message}));
 });
