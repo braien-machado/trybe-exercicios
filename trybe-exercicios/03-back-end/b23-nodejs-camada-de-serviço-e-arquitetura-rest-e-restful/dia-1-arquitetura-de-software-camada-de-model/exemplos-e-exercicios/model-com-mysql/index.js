a9fb316752cb863f759c6c3603fde25e8fcdfd12
Braien Machado de Paula <braienmp@outlook.com>
2022-02-17 19:52:41 -0300

    separa path /books em middleware de rota

--- trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exemplos-e-exercicios/model-com-mysql/index.js
+++ trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exemplos-e-exercicios/model-com-mysql/index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const authorRoute = require('./routes/authorRoute');
+const bookRoute = require('./routes/bookRoute');
 
 const app = express();
 const port = 3000;
@@ -8,42 +9,7 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.use('/authors', authorRoute);
-
-const Book = require('./models/Book');
-
-app.get('/books', async (req, res) => {
-  const { id } = req.query;
-  if (id) {
-    const booksById = await Book.getByAuthorId(id);
-    return res.status(200).json(booksById);
-  }
-  const books = await Book.getAll();
-
-  res.status(200).json(books);
-});
-
-app.get('/books/:id', async (req, res) => {
-  const { id } = req.params;
-  const book = await Book.findById(id);
-
-  if (!book) return res.status(404).json({ message: 'Not found' });
-
-  res.status(200).json(book);
-});
-
-app.post('/books', async (req, res) => {
-  try {
-    const { title, author_id } = req.body;
-  
-    if (!(await Book.isValid(title, author_id))) return res.status(400).json({ message: 'Dados inválidos' });
-  
-    await Book.create(title, author_id);
-  
-    res.status(201).json({message: 'Livro criado com sucesso!'});
-  } catch (error) {
-    next(error);
-  }
-});
+app.use('/books', bookRoute)
 
 app.use((err, req, res, next) => {
   res.status(err.code).json(err.message);
