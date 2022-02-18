db008394ff6c69c45bafe9c7a314ffbdee0dcb98
Braien Machado de Paula <braienmp@outlook.com>
2022-02-18 18:21:02 -0300

    instala dotenv, implementa utilizacao e o exclui de versionamento

--- trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplos/hello-msc/index.js
+++ trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplos/hello-msc/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const Author = require('./controllers/Author');
 const errorMiddleware = require('./middlewares/error');
@@ -16,7 +17,7 @@ app.post('/authors', Author.createAuthor);
 
 app.use(errorMiddleware);
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Ouvindo a porta ${PORT}`);
--- trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplos/hello-msc/models/connection.js
+++ trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplos/hello-msc/models/connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'braien',
-  password: 'Mysql@2022',
-  database: 'model_example'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
 });
 
 module.exports = connection;
