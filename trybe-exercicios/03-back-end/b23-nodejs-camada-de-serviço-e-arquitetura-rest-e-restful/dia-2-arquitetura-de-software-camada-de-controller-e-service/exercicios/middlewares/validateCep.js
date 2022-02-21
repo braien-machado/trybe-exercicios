15df8090a6ed1454f0d8d77d94f0b0eddbfa23a7
Braien Machado de Paula <braienmp@outlook.com>
2022-02-21 13:28:00 -0300

    move geracao de mensagem de erro para pasta /services

--- trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/middlewares/validateCep.js
+++ trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/middlewares/validateCep.js
@@ -3,13 +3,10 @@ const Cep = require('../services/cepService');
 const validateCep = (req, res, next) => {
   const { cep } = req.params;
 
-  if (!Cep.isCepValid(cep)) {
-    return res.status(400).json({
-      error: {
-        code: 'invalidData',
-        message: 'CEP inválido',
-      },
-    });
+  const { error } = Cep.isCepValid(cep);
+
+  if (error) {
+    return res.status(400).json(error);
   }
 
   return next();
