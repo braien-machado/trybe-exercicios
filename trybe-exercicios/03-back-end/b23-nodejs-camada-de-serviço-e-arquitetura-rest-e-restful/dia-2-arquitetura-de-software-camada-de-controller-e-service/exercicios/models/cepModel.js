3f580760c1b5d00dd06612d61c24d3c8fbf8254e
Braien Machado de Paula <braienmp@outlook.com>
2022-02-21 22:00:41 -0300

    implementa model para criacao de cep

--- trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/models/cepModel.js
+++ trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/models/cepModel.js
@@ -12,6 +12,21 @@ const getCep = async (cepNumber) => {
   return cep;
 };
 
+const createCep = async ({
+  cep, logradouro, bairro, localidade, uf,
+}) => {
+  const query = (
+    `INSERT INTO cep_lookup.ceps ('cep','logradouro','bairro','localidade','uf')
+    VALUES(?,?,?,?,?)`
+  );
+
+  const created = await connection.execute(query, [cep, logradouro, bairro, localidade, uf]);
+
+  console.log(created);
+  return created;
+};
+
 module.exports = {
   getCep,
+  createCep,
 };
