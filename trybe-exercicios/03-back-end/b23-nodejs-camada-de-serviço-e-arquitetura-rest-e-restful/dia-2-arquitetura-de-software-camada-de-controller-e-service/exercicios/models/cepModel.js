03a41b2e300e59430ffabb1fa487bace2e630857
Braien Machado de Paula <braienmp@outlook.com>
2022-02-21 22:37:56 -0300

    corrige query em createCep

--- trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/models/cepModel.js
+++ trybe-exercicios/03-back-end/b23-nodejs-camada-de-serviço-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/models/cepModel.js
@@ -16,13 +16,12 @@ const createCep = async ({
   cep, logradouro, bairro, localidade, uf,
 }) => {
   const query = (
-    `INSERT INTO cep_lookup.ceps ('cep','logradouro','bairro','localidade','uf')
-    VALUES(?,?,?,?,?)`
+    `INSERT INTO cep_lookup.ceps (cep,logradouro,bairro,localidade,uf)
+    VALUES(?,?,?,?,?);`
   );
 
   const created = await connection.execute(query, [cep, logradouro, bairro, localidade, uf]);
 
-  console.log(created);
   return created;
 };
 
