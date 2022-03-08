db01ff1affaa7df0e25b6391b0c55bd4e2369c59
Braien Machado de Paula <braienmp@outlook.com>
2022-03-08 18:24:35 -0300

    realiza exemplos do conteudo do dia

---
+++ trybe-exercicios/03-back-end/b24-nodejs-orm-e-autenticacao/dia-2-orm-associations/exemplos/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "braien",
+    "password": "Mysql@2022",
+    "database": "associations",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+};
