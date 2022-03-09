a848f1eebac32d9a82cd11da5b8fba28c97412b1
Braien Machado de Paula <braienmp@outlook.com>
2022-03-09 19:54:07 -0300

    adiciona verificacao de login do administrador

--- trybe-exercicios/03-back-end/b24-nodejs-orm-e-autenticacao/dia-3-JWT-JSON-Web-Token/hello-jwt/controllers/login.js
+++ trybe-exercicios/03-back-end/b24-nodejs-orm-e-autenticacao/dia-3-JWT-JSON-Web-Token/hello-jwt/controllers/login.js
@@ -1,25 +1,16 @@
 const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-const { string } = require('joi');
-const secret = process.env.SECRET;
-
-const schema = Joi.object({
-  username: Joi.string().alphanum().min(5).required(),
-  password: Joi.string().min(5).required(),
-});
+const { env: {SECRET, ADMIN_PASSWORD, ADMIN_USERNAME } } = process;
 
 module.exports = (req, res, next) => {
   const { username, password } = req.body;
-  const { error } = schema.validate({ username, password })
   
-  if (error) return next(error);
-
   const jwtConfig = {
     expiresIn: '1h',
     algorithm: 'HS256',
   };
 
-  const token = jwt.sign({ data: username, admin: false }, secret, jwtConfig);
+  const admin = (username === ADMIN_USERNAME && password === ADMIN_PASSWORD);
+  const token = jwt.sign({ data: username, admin }, SECRET, jwtConfig);
 
   res.status(200).json({ token });
 };
