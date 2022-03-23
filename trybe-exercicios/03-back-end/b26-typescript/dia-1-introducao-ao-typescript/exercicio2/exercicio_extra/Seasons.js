f6b8db1f04ee8def3bfd316934e47b1603075fe5
Braien Machado de Paula <braienmp@outlook.com>
2022-03-23 19:16:04 -0300

    finaliza exercicios do dia

---
+++ trybe-exercicios/03-back-end/b26-typescript/dia-1-introducao-ao-typescript/exercicio2/exercicio_extra/Seasons.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Seasons;
+(function (Seasons) {
+    Seasons["OUTONO"] = "Outono";
+    Seasons["INVERNO"] = "Inverno";
+    Seasons["PRIMAVERA"] = "Primavera";
+    Seasons["VER\u00C3O"] = "Ver\u00E3o";
+})(Seasons || (Seasons = {}));
+exports.default = Seasons;
